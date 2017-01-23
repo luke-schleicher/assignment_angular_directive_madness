f1c220a303cc68fc27e3e3de34c8cb67e168a1eb
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-23 13:54:51 -0500

    done up to validations

--- js/quotes.js
+++ js/quotes.js
@@ -3,15 +3,32 @@ directiveMadness.controller('quoteCtrl',
   function($scope) {
 
     $scope.quotes = [];
+    $scope.newQuote = {};
+    $scope.id = 0
+
+    $scope.createQuote = function(){
+      var quote = {
+        body: $scope.newQuote.body,
+        author: $scope.newQuote.author,
+        id: $scope.id
+      }
+      ++$scope.id
+      $scope.newQuote = {}
+      $scope.quotes.push(quote)
+    };
+
+    $scope.deleteQuote = function(id){
+      for(var i = 0; i < $scope.quotes.length; i++){
+        if($scope.quotes[i].id === id){
+          $scope.quotes.splice(i, 1)
+          break;
+        }
+      }
+    }
 
   }]);
 
 directiveMadness.directive('quoteForm', function() {
-
-  var addQuote = function(scope, element, attributes) {
-    var quote = {};
-    var 
-  };
 
   return {
     templateUrl: 'quoteForm.html',
