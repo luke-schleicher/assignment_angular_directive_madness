b359c49035aa71d702d6b2c9f6ec2a7da0f913ff
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-23 16:47:17 -0600

    scope education

--- js/scopes.js
+++ js/scopes.js
@@ -1,6 +1,33 @@
 directiveMadness.controller('scopesCtrl',
   ['$scope',
-  function($scope) {
-    $scope.string1 = "first"
-    $scope.string2 = "second"
-  }]);
+    function($scope) {
+      $scope.twoWay = "first";
+      $scope.oneWay = "second";
+    }
+  ]
+);
+
+
+
+directiveMadness.directive('isolated', function() {
+
+  var linkFunc = function(scope) {
+    scope.twoWay = "I have been modified";
+    scope.oneWay = "I too have been modified";
+    scope.sayHello = function(name) {
+      alert('hello ' + name);
+    };
+  };
+
+  return {
+    templateUrl: "isolated.html",
+    restrict: 'AE',
+    scope: {
+      twoWay: '=',
+      oneWay: '@',
+      sayHello: '&'
+    },
+    link: linkFunc
+  };
+
+});
