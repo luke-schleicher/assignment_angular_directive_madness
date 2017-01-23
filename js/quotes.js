156c59290de525f2065e37bf1816d35dd94b3130
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-23 12:32:59 -0600

    quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,32 @@
 directiveMadness.controller('quoteCtrl',
   ['$scope',
   function($scope) {
+
+    $scope.quotes = [];
+
   }]);
+
+directiveMadness.directive('quoteForm', function() {
+
+  var addQuote = function(scope, element, attributes) {
+    var quote = {};
+    var 
+  };
+
+  return {
+    templateUrl: 'quoteForm.html',
+    restrict: 'E',
+    scope: true
+  };
+
+});
+
+directiveMadness.directive('quotesIndex', function() {
+
+  return {
+    templateUrl: 'quotesIndex.html',
+    restrict: 'E',
+    scope: true
+  }
+
+});
